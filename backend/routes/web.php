e7af0d905d5f540e3067a4e32d97b11ea4e05a34
Sergei Ten <sergei.ten@gmail.com>
2025-03-26 20:03:17 +0900

    Add api routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
-
-Route::resource('post', PostController::class);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
